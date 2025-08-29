1019e369c75764a89a3552587d5ead6dce885614
couvq <quentincouvelaire@gmail.com>
2025-08-29 16:36:13 -0400

    messing around with uninitialized variables

--- 0.7_Compiling your first program/main.cpp
+++ 0.7_Compiling your first program/main.cpp
@@ -1,9 +1,14 @@
-//
-// Created by Quentin Couvelaire on 8/27/25.
-//
 #include <iostream>
 
 int main() {
     std::cout << "Hello, world!";
+    int x;
+    /*
+     * This is an interesting behavior of C++. Uninitialized variables contain
+     * whatever garbage value was in the memory address of the variable. So this line prints
+     * out a number and it changes each time the program runs (as it claims a new memory address).
+     * I can print out the memory address of the variable by changing it to a reference.
+     */
+    std::cout << x;
     return 0;
 }
