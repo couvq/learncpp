bffc03d1ecb73bb5d8fc9bd128995f16f799c310
couvq <quentincouvelaire@gmail.com>
2025-08-30 16:39:07 -0400

    lesson 1.5 quiz

--- 0.7_Compiling your first program/main.cpp
+++ 0.7_Compiling your first program/main.cpp
@@ -2,22 +2,12 @@
 
 int main()
 {
-    int x;
-    /*
-     * This is an interesting behavior of C++. Uninitialized variables contain
-     * whatever garbage value was in the memory address of the variable. So this line prints
-     * out a number, and it changes each time the program runs (as it claims a new memory address).
-     * I can print out the memory address of the variable by changing it to a reference.
-     */
-    std::cout << x << '\n';
+    int x {};
+    int y {};
+    int z {};
 
-    double pi { 3.14159 };
-    /* 
-        Using "\n" is technically more efficient than std::endl
-        as endl adds a new line and flushes the buffer for cout. \n simply 
-        adds a new line and lets c++ output system flush the buffer manually.
-        https://www.learncpp.com/cpp-tutorial/introduction-to-iostream-cout-cin-and-endl/
-    */
-    std::cout << pi << '\n'; 
+    std::cout << "Enter three numbers: ";
+    std::cin >> x >> y >> z;
+    std::cout << "You entered " << x << ", " << y << ", and " << z << ".\n";
     return 0;
 }
