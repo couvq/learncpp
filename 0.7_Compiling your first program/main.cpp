67bf4f8d44bc409269c6a3335cf405000ceebff6
couvq <quentincouvelaire@gmail.com>
2025-08-31 14:34:34 -0400

    c++ has ternary operator

--- 0.7_Compiling your first program/main.cpp
+++ 0.7_Compiling your first program/main.cpp
@@ -2,12 +2,20 @@
 
 int main()
 {
-    int x {};
-    int y {};
-    int z {};
+    int x{};
+    int y{};
+    int z{};
 
     std::cout << "Enter three numbers: ";
     std::cin >> x >> y >> z;
     std::cout << "You entered " << x << ", " << y << ", and " << z << ".\n";
+    /* 
+        Called a ternary operator because it has three operands.
+        1. condition
+        2. result if condition is true
+        3. result if condition is false
+    */
+    int res { true == false ? 5 : 10};
+    std::cout << res << "\n";
     return 0;
 }
