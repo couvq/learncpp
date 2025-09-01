f12db3b1813e05923c2a6744ceffcc12b53e870a
couvq <quentincouvelaire@gmail.com>
2025-09-01 10:38:59 -0400

    chapter 1 quiz question 3

--- 1.11_Developing your first program/main.cpp
+++ 1.11_Developing your first program/main.cpp
@@ -9,7 +9,19 @@ void multiply_by_two()
     std::cout << "Triple " << x << " is: " << x * 3 << "\n";
 }
 
+void sum_and_difference()
+{
+    int x{};
+    int y{};
+    std::cout << "Enter two numbers, separated by spaces: ";
+    std::cin >> x >> y;
+    std::cout << x << " + " << y << " is " << x + y << "\n";
+    std::cout << x << " - " << y << " is " << x - y << "\n";
+}
+
 int main()
 {
-    multiply_by_two();
+    // multiply_by_two();
+    sum_and_difference();
+    return 0;
 }
