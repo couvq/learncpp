c5fc264eab77ae6d3be69e0896319719e74553b8
couvq <quentincouvelaire@gmail.com>
2025-09-01 10:29:15 -0400

    extended multiply_by_two per lesson exercise

--- 1.11_Developing your first program/main.cpp
+++ 1.11_Developing your first program/main.cpp
@@ -5,7 +5,8 @@ void multiply_by_two()
     int x{};
     std::cout << "Enter an integer: ";
     std::cin >> x;
-    std::cout << "Double that number is: " << x * 2 << "\n";
+    std::cout << "Double " << x << " is: " << x * 2 << "\n";
+    std::cout << "Triple " << x << " is: " << x * 3 << "\n";
 }
 
 int main()
