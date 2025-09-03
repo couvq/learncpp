0ed0753db40ea83f0925164db3712e5c97160328
couvq <quentincouvelaire@gmail.com>
2025-09-03 15:56:46 -0400

    pass by value example

--- 1.11_Developing your first program/main.cpp
+++ 1.11_Developing your first program/main.cpp
@@ -8,12 +8,24 @@ int getInputFromUser()
     return input;
 }
 
+void printSum(int x, int y)
+{
+    std::cout << "printSum's x and y in memory: " << &x << " " << &y << "\n";
+    std::cout << x << " + " << y << " is " << x + y << "\n";
+}
+
+void printDifference(int x, int y)
+{
+    std::cout << x << " - " << y << " is " << x - y << "\n";
+}
+
 void sum_and_difference()
 {
     int x{getInputFromUser()};
     int y{getInputFromUser()};
-    std::cout << x << " + " << y << " is " << x + y << "\n";
-    std::cout << x << " - " << y << " is " << x - y << "\n";
+    std::cout << "initial x and y in memory: " << &x << " " << &y << "\n";
+    printSum(x, y);        // pass by value, value is copied to make local function variables x & y
+    printDifference(x, y); // pass by value, value is copied to make local function variables x & y
 }
 
 int main()
